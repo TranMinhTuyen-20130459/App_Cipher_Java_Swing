9546b6c129f5f55b65e1be6afba2e99bddbe77f0
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-10-14 11:47:39 +0700

    tiếp tục thêm giao diện cho component MA_HOA_DOI_XUNG

--- src/it-nlu/java/view/Panel_MA_HOA_DOI_XUNG.java
+++ src/it-nlu/java/view/Panel_MA_HOA_DOI_XUNG.java
@@ -3,9 +3,10 @@ package view;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
+import java.util.Arrays;
 
 public class Panel_MA_HOA_DOI_XUNG extends JPanel {
-    private JButton bt_encrypt, bt_decrypt;
+    private JButton bt_encrypt, bt_decrypt, bt_check_key;
     private JTextArea plain_text_area,
             encrypted_text_area,
             decrypted_text_area;
@@ -16,6 +17,13 @@ public class Panel_MA_HOA_DOI_XUNG extends JPanel {
     private JScrollPane scroll_pane_plain_text_area,
             scroll_pane_encrypted_text_area,
             scroll_pane_decrypted_text_area;
+
+    private JTextField key_text_field;
+    private JComboBox combo_box_algorithm, combo_box_language;
+
+    private String[] arr_algorithms = {"Vigenere", "Hill", "Affine", "DES", "Triple DES", "AES"};
+
+    private String[] arr_languages = {"VietNam", "English"};
 
     public Panel_MA_HOA_DOI_XUNG(int WIDTH, int HEIGHT) {
 
@@ -37,31 +45,44 @@ public class Panel_MA_HOA_DOI_XUNG extends JPanel {
         text_chon_ngon_ngu.setFont(new Font("Arial", Font.PLAIN, 14));
         text_chon_ngon_ngu.setBounds(364, 19, 119, 37);
 
+        JLabel text_nhap_key = new JLabel("Key:");
+        text_nhap_key.setForeground(Color.BLACK);
+        text_nhap_key.setHorizontalAlignment(SwingConstants.CENTER);
+        text_nhap_key.setFont(new Font("Arial", Font.PLAIN, 14));
+        text_nhap_key.setBounds(0, 83, 80, 37);
+
         JLabel text_van_ban_ma_hoa = new JLabel("Đoạn văn bản mã hóa:");
         text_van_ban_ma_hoa.setForeground(Color.BLACK);
         text_van_ban_ma_hoa.setHorizontalAlignment(SwingConstants.CENTER);
         text_van_ban_ma_hoa.setFont(new Font("Arial", Font.PLAIN, 14));
-        text_van_ban_ma_hoa.setBounds(19, 196, 149, 90);
+        text_van_ban_ma_hoa.setBounds(19, 240, 143, 70);
 
         JLabel text_van_ban_giai_ma = new JLabel("Đoạn văn bản giải mã:");
         text_van_ban_giai_ma.setForeground(Color.BLACK);
         text_van_ban_giai_ma.setHorizontalAlignment(SwingConstants.CENTER);
         text_van_ban_giai_ma.setFont(new Font("Arial", Font.PLAIN, 14));
-        text_van_ban_giai_ma.setBounds(17, 318, 151, 90);
+        text_van_ban_giai_ma.setBounds(17, 330, 145, 70);
 
         add(text_chon_giai_thuat);
         add(text_chon_ngon_ngu);
+        add(text_nhap_key);
         add(text_van_ban_ma_hoa);
         add(text_van_ban_giai_ma);
 
         createTextAreaGroup();
         createButtonGroup();
+        createComboBoxGroup();
+        createTextFieldGroup();
 
         add(scroll_pane_plain_text_area);
         add(scroll_pane_encrypted_text_area);
         add(scroll_pane_decrypted_text_area);
         add(bt_encrypt);
         add(bt_decrypt);
+        add(bt_check_key);
+        add(combo_box_algorithm);
+        add(combo_box_language);
+        add(key_text_field);
     }
 
     public void createButtonGroup() {
@@ -70,6 +91,9 @@ public class Panel_MA_HOA_DOI_XUNG extends JPanel {
 
         bt_decrypt = new RoundedButton("GIẢI MÃ", 25, new Color(217, 217, 217));
         bt_decrypt.setBounds(499, 439, 106, 26);
+
+        bt_check_key = new RoundedButton("NHẬP KEY", 35, new Color(58, 205, 34));
+        bt_check_key.setBounds(510, 82, 101, 37);
     }
 
     public void createTextAreaGroup() {
@@ -83,7 +107,7 @@ public class Panel_MA_HOA_DOI_XUNG extends JPanel {
         plain_text_area.setBackground(new Color(217, 217, 217));
 
         scroll_pane_plain_text_area = new JScrollPane(plain_text_area);
-        scroll_pane_plain_text_area.setBounds(19, 78, 592, 90);
+        scroll_pane_plain_text_area.setBounds(19, 150, 592, 70);
 
         // Thiết lập hiển thị thanh cuộn theo cần thiết
         scroll_pane_plain_text_area.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
@@ -98,7 +122,7 @@ public class Panel_MA_HOA_DOI_XUNG extends JPanel {
         encrypted_text_area.setBackground(new Color(217, 217, 217));
 
         scroll_pane_encrypted_text_area = new JScrollPane(encrypted_text_area);
-        scroll_pane_encrypted_text_area.setBounds(168, 196, 443, 90);
+        scroll_pane_encrypted_text_area.setBounds(190, 240, 420, 70);
 
         // Thiết lập hiển thị thanh cuộn theo cần thiết
         scroll_pane_encrypted_text_area.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
@@ -115,7 +139,7 @@ public class Panel_MA_HOA_DOI_XUNG extends JPanel {
         decrypted_text_area.setBackground(new Color(217, 217, 217));
 
         scroll_pane_decrypted_text_area = new JScrollPane(decrypted_text_area);
-        scroll_pane_decrypted_text_area.setBounds(168, 318, 443, 90);
+        scroll_pane_decrypted_text_area.setBounds(190, 330, 420, 70);
 
         // Thiết lập hiển thị thanh cuộn theo cần thiết
         scroll_pane_decrypted_text_area.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
@@ -127,6 +151,23 @@ public class Panel_MA_HOA_DOI_XUNG extends JPanel {
         decrypted_text_area.setEnabled(false);
     }
 
+    public void createComboBoxGroup() {
+
+        Arrays.sort(arr_algorithms);
+        combo_box_algorithm = new JComboBox<>(arr_algorithms);
+        combo_box_language = new JComboBox<>(arr_languages);
+
+        combo_box_algorithm.setBounds(145, 19, 210, 38);
+        combo_box_language.setBounds(483, 20, 128, 34);
+    }
+
+    public void createTextFieldGroup() {
+        key_text_field = new JTextField();
+        key_text_field.setBounds(145, 82, 338, 37);
+        key_text_field.setFont(new Font("Arial", Font.PLAIN, 14));
+        key_text_field.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        key_text_field.setEditable(false);
+    }
 
     public class RoundedButton extends JButton {
         private int radius;
@@ -175,7 +216,7 @@ public class Panel_MA_HOA_DOI_XUNG extends JPanel {
             if (getText().isEmpty() && !isFocusOwner()) {
                 g.setColor(Color.GRAY);
                 g.setFont(new Font("Arial", Font.BOLD, 16));
-                g.drawString(placeholder, getInsets().left + 150, g.getFontMetrics().getHeight() + getInsets().top + 30);
+                g.drawString(placeholder, getInsets().left + 150, g.getFontMetrics().getHeight() + getInsets().top + 18);
             }
         }
     }
