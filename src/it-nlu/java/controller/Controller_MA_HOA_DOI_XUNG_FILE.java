9fa7c60bdf4c87d9ec225c1300ebf656c586025a
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-10-16 22:45:48 +0700

    thêm Controller để điều phối giữa Model và View

---
+++ src/it-nlu/java/controller/Controller_MA_HOA_DOI_XUNG_FILE.java
@@ -0,0 +1,4 @@
+package controller;
+
+public class Controller_MA_HOA_DOI_XUNG_FILE {
+}
---
+++ src/it-nlu/java/controller/Controller_MA_HOA_DOI_XUNG_TEXT.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javax.crypto.SecretKey;
+
+public class Controller_MA_HOA_DOI_XUNG_TEXT {
+
+    public static String encryptText(String algorithm,
+                                     String language,
+                                     String plain_text,
+                                     SecretKey key) {
+        return "Tran Minh Tuyen";
+    }
+
+}
