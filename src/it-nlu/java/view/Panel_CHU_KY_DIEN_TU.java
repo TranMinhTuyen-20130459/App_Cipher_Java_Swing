256b97500d03d4dbfbcdb856ecfc7923c3d2351d
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-11-08 13:27:53 +0700

    thêm giao diện cho chức năng chữ ký điện tử

--- src/it-nlu/java/view/Panel_CHU_KY_DIEN_TU.java
+++ src/it-nlu/java/view/Panel_CHU_KY_DIEN_TU.java
@@ -2,24 +2,213 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.RoundRectangle2D;
+import java.io.File;
 
 public class Panel_CHU_KY_DIEN_TU extends JPanel {
+
+    private JLabel lb_input,
+            lb_chon_giai_thuat,
+            lb_name_file;
+    private JScrollPane scroll_pane_input_text_area;
+    private JTextArea input_text_area;
+    private JComboBox combo_box_algorithm;
+
+    private JButton bt_choose_file, bt_check;
+
+    private JPanel panel_choose_file;
+    private final String[] arr_algorithms = {"MD5", "SHA-1", "SHA-224", "SHA-256", "SHA-384", "SHA-512"};
+
+    private String name_algorithm = "";
 
     public Panel_CHU_KY_DIEN_TU(int WIDTH, int HEIGHT) {
 
         setBackground(new Color(255, 255, 255, 255));
+
         setBounds(0, 0, WIDTH, HEIGHT);
         setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
         setLayout(null);
         setVisible(true);
 
-        JLabel text = new JLabel("CHỮ KÝ ĐIỆN TỬ");
-        text.setForeground(Color.BLACK);
-        text.setHorizontalAlignment(SwingConstants.CENTER);
-        text.setFont(new Font("Arial", Font.PLAIN, 16));
-        text.setBounds(0, 25, WIDTH / 3, 40);
+        createLabelGroup();
+        createButtonGroup();
+        createComboBoxGroup();
+        createTextAreaGroup();
+        createPanelChooseFile();
 
-        add(text);
+        add(panel_choose_file);
+
+        add(lb_input);
+        add(lb_chon_giai_thuat);
+
+        add(scroll_pane_input_text_area);
+
+        add(combo_box_algorithm);
+
+        add(bt_check);
+    }
+
+    public void createLabelGroup() {
+
+        lb_input = new JLabel("Input:");
+        lb_input.setForeground(Color.BLACK);
+        lb_input.setHorizontalAlignment(SwingConstants.LEFT);
+        lb_input.setFont(new Font("Arial", Font.PLAIN, 14));
+        lb_input.setBounds(23, 168, 46, 15);
+
+        lb_chon_giai_thuat = new JLabel("Chọn giải thuật:");
+        lb_chon_giai_thuat.setForeground(Color.BLACK);
+        lb_chon_giai_thuat.setHorizontalAlignment(SwingConstants.LEFT);
+        lb_chon_giai_thuat.setFont(new Font("Arial", Font.PLAIN, 14));
+        lb_chon_giai_thuat.setBounds(23, 30, 110, 22);
 
     }
+
+    public void createComboBoxGroup() {
+        createComboBoxAlgorithm();
+    }
+
+    public void createComboBoxAlgorithm() {
+        combo_box_algorithm = new JComboBox(arr_algorithms);
+        combo_box_algorithm.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        combo_box_algorithm.setFont(new Font("Arial", Font.PLAIN, 14));
+        combo_box_algorithm.setBounds(128, 25, 200, 38);
+
+        name_algorithm = combo_box_algorithm.getSelectedItem().toString();
+        combo_box_algorithm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                name_algorithm = combo_box_algorithm.getSelectedItem().toString();
+            }
+        });
+
+    }
+
+    public void createTextAreaGroup() {
+        createInputTextArea();
+    }
+
+    private void createInputTextArea() {
+
+        input_text_area = new JTextArea();
+        input_text_area.setFont(new Font("Arial", Font.PLAIN, 16));
+        input_text_area.setBackground(new Color(217, 217, 217));
+
+        scroll_pane_input_text_area = new JScrollPane(input_text_area);
+        scroll_pane_input_text_area.setBounds(23, 185, 435, 109);
+
+        // Thiết lập hiển thị thanh cuộn theo cần thiết
+        scroll_pane_input_text_area.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        // Đảm bảo thanh cuộn sẽ hiển thị khi nội dung vượt quá kích thước của JTextArea
+        input_text_area.setLineWrap(true);
+        input_text_area.setWrapStyleWord(true);
+
+
+    }
+
+    public void createButtonGroup() {
+        createButtonCheck();
+    }
+
+    public void createButtonCheck() {
+        bt_check = new RoundedButton("KIỂM TRA", 0, new Color(9, 135, 232));
+        bt_check.setForeground(Color.WHITE);
+        bt_check.setFont(new Font("Arial", Font.BOLD, 12));
+        bt_check.setBounds(494, 185, 123, 109);
+        bt_check.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+    }
+
+    public void createPanelChooseFile() {
+        panel_choose_file = new JPanel();
+        panel_choose_file.setBounds(23, 83, 594, 54);
+        panel_choose_file.setBackground(new Color(217, 217, 217));
+        panel_choose_file.setLayout(null);
+        panel_choose_file.setVisible(true);
+
+        lb_name_file = new JLabel("");
+        lb_name_file.setBounds(125, 20, 500, 15);
+        panel_choose_file.add(lb_name_file);
+
+        createButtonChooseFile();
+        panel_choose_file.add(bt_choose_file);
+    }
+
+    public void createButtonChooseFile() {
+
+        bt_choose_file = new RoundedButton("CHỌN FILE", 10, new Color(215, 187, 18));
+        bt_choose_file.setBounds(10, 12, 100, 30);
+
+        // Thêm sự kiện cho nút "CHỌN FILE"
+        bt_choose_file.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+
+                int returnValue = fileChooser.showOpenDialog(null); // Hiển thị cửa sổ chọn tệp
+
+                /***
+                 - JFileChooser.APPROVE_OPTION: Được trả về khi người dùng đã chọn một tệp hoặc thư mục và xác nhận việc chọn (thường bằng cách nhấn nút "Open").
+                 - JFileChooser.CANCEL_OPTION: Được trả về khi người dùng đã hủy việc chọn hoặc đóng cửa sổ chọn tệp.
+                 - JFileChooser.ERROR_OPTION: Được trả về nếu có lỗi xảy ra trong quá trình chọn tệp.
+                 */
+
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    // Lấy tệp đã chọn
+                    File selectedFile = fileChooser.getSelectedFile();
+
+
+                    // System.out.println("Đường dẫn tuyệt đối đến tệp: " + path_selected_file);
+                    // System.out.println("Đường dẫn đến thư mục chứa tệp: " + path_folder_contain_selected_file);
+                }
+            }
+        });
+
+    }
+
+    public class RoundedButton extends JButton {
+        private int radius;
+        private Color backgroundColor;
+
+        public RoundedButton(String text, int radius, Color backgroundColor) {
+            super(text);
+            this.radius = radius;
+            this.backgroundColor = backgroundColor;
+            setContentAreaFilled(false);
+            setFocusPainted(false);
+            setBorderPainted(false);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            if (getModel().isArmed()) {
+                g.setColor(backgroundColor.darker());
+            } else {
+                g.setColor(backgroundColor);
+            }
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.fill(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), radius, radius));
+            g2d.setColor(Color.BLACK); // Màu viền đen
+            g2d.draw(new RoundRectangle2D.Double(0, 0, getWidth() - 1, getHeight() - 1, radius, radius));
+            super.paintComponent(g);
+        }
+
+        @Override
+        protected void paintBorder(Graphics g) {
+            // Do nothing to hide the default button border
+        }
+    }
+
+
 }
